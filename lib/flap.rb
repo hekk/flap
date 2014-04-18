b74264d5b2270b4a281bd898cf580df7cd96899a
Tomohiro Nishimura <tomohiro68@gmail.com>
2014-04-18 12:57:15 +0900

    Write documentation

--- lib/flap.rb
+++ lib/flap.rb
@@ -5,23 +5,45 @@ module Flap
     Module.__send__ :include, ModuleMethods
   end
 
+  # Public: tap + instance_eval
+  #
+  # block - call in receiver's context
+  #
+  # Examples
+  #   hash = {a: 1, b: 2}
+  #   hash.instance_tap_eval { delete :a } #=> {:b => 2}
+  #
+  # Returns self
   def instance_tap_eval(&block)
     self.instance_eval &block if block_given?
     self
   end
 
+  # Deprecated: same as instance_tap_eval
   def flap(&block)
     warn "[deprecated] this method will be removed."
     instance_tap_eval &block
   end
 
   module ModuleMethods
+    # Public: tap + class_eval
+    #
+    # block - call in class context
+    #
+    # Examples
+    #   Klass = Class.new.class_tap_eval { include Awesome }
+    #   Klass.new.awesome_method
+    #
+    # Returns self
     def class_tap_eval(&block)
       class_eval &block if block_given?
       self
     end
   end
 
+  # Public: define alias methods itap/ctap.
+  #
+  # Returns self
   def self.enable_short_methods!
     Object.__send__ :include, ShortMethods
   end
